484ea57c0bb99bc83000ae32444fb4c984ef578b
jan-albatros <51405507+jan-albatros@users.noreply.github.com>
2019-06-07 20:26:26 +0200

    Primary logic and math operations (JS).
    JavaScript added for simple math operations. Some HTML and CSS/SCSS tweaks.

--- app/js/flex_calc.js
+++ app/js/flex_calc.js
@@ -1,58 +1,111 @@
-//flex_calc.js
+/* flex_calc.js –– simple calculator
+ *
+ *	Adds, substracts, multiplies, divides one-digit integer numbers. Resets prints output.
+ *
+ *	TO DO: 	a) process multi-digit numbers (arrays),
+ *			b) process float numbers,
+ * 			c) output operands and operator / temp sum in "history" output
+ *			d) save result to CSV file (for further processing with PHP).
+ *
+ */
 
-/*
-var btn_1 = document.getElementById('btn_1');
 
-btn_1.addEventListener('click', event => {
-	console.log(1);
-})
-*/
+/* Set global vars */
+var result = 0;				// result of the math operation
+var firstOperand = null; 	// first operand
+var operator = null;		// operator +,-,/,%
+var nextOperand = null; 	// second/next operand
+var memory = 0;				// temp for saving result
+var output;					// output result for user
+
+
+// Add listeners to numerals and operators by type   
+document.addEventListener('click', function (event) {
+	
+	// Process numerals
+	if (event.target.matches('.num')) {
+		//console.log(' Number button clicked ');
+		console.log(event.target.innerText);
+
+		// if first operand is null write, to it. if it's defined, write to next/second operand
+		if (firstOperand == null) {
+			firstOperand = Number(event.target.innerText);
+		} else {
+			nextOperand = Number(event.target.innerText);
+		}
+		//console.log('firstOperand: ' + firstOperand + ', nextOperand: ' + nextOperand);
+	}
+
+	// Process operators
+	if (event.target.matches('.opp')) {
+		
+		if (firstOperand == null) {
+			firstOperand = 0;
+		} 
+		//operator = event.target.innerText;
+		operator = event.target.dataset.operator; // get value of 'data-operator' attr of HTML element
+		console.log(operator);
+	}
+}, false);
+
+// Bind actions to other keys when window loads and specify our functions
 window.onload=function(){
 	
-	var result = 0;
-	var firstOperand = null;
-	var operator = null;
-	var nextOperand = null;
+	// Process special keys
+	// Equals – get result of the math operation
+	document.getElementById('btn_equals').addEventListener('click', event => {
+		
+		result = calculate(); // calculate 
+		refreshOutput(); // print output
+		console.log('result: ' + result);
+	});
 
-	const btn0 = document.getElementById('btn_0');
-	const btn1 = document.getElementById('btn_1');
-	const btn2 = document.getElementById('btn_2');
-	const btn3 = document.getElementById('btn_3');
-	const btn4 = document.getElementById('btn_4');
-	const btn5 = document.getElementById('btn_5');
-	const btn6 = document.getElementById('btn_6');
-	const btn7 = document.getElementById('btn_7');
-	const btn8 = document.getElementById('btn_8');
-	const btn9 = document.getElementById('btn_9');
+	// AC - reset
+	document.getElementById('btn_ac').addEventListener('click', event => {
+		
+		result = 0;
+		firstOperand = null;
+		operator = null;
+		nextOperand = null;
+		refreshOutput(); // print output
+		console.log('RESET. Result: ' + result);
+	});
 
-	btn0.addEventListener('click', event => {
-		console.log(0);
+	// Save - Save to memory (to be continued – save to CSV file with IP address, browser agent name and datetime)
+	document.getElementById('btn_save').addEventListener('click', event => {
+		// ...
+		memory = result;
+		console.log('SAVED. Result saved: ' + result);
 	});
-	btn1.addEventListener('click', event => {
-		console.log(1);
-	});
-	btn2.addEventListener('click', event => {
-		console.log(2);
-	});
-	btn3.addEventListener('click', event => {
-		console.log(3);
-	});
-	btn4.addEventListener('click', event => {
-		console.log(4);
-	});
-	btn5.addEventListener('click', event => {
-		console.log(5);
-	});
-	btn6.addEventListener('click', event => {
-		console.log(6);
-	});
-	btn7.addEventListener('click', event => {
-		console.log(7);
-	});
-	btn8.addEventListener('click', event => {
-		console.log(8);
-	});
-	btn9.addEventListener('click', event => {
-		console.log(9);
-	});
+
+	// Perform math operation on operands
+	function calculate(){
+		
+		console.log('CALCULATE: ' + firstOperand + ' ' + operator + ' ' + nextOperand);
+		switch(operator){
+			case "+": /* plus */ 
+				result = firstOperand + nextOperand;
+				break;
+			case "-": /* minus */
+				result = firstOperand - nextOperand;
+				break;
+			case "*": /* times / multiply */
+				result = firstOperand * nextOperand;
+				break;
+			case "/": /* divide */
+				result = firstOperand / nextOperand;
+				break;
+			default: /* unknown operation - print Error */
+				result = "ERR";
+				break;
+		}
+		return result;
+	}
+
+	function refreshOutput(){
+
+		output = document.getElementById('disp_output');
+		output.innerHTML = result;
+
+	}
 }
