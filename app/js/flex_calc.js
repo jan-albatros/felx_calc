c24ba55727d9e4871838d9bbaca8389239cd2a63
jan-albatros <51405507+jan-albatros@users.noreply.github.com>
2019-06-10 13:41:50 +0200

    JS & logic ready.
    Logic ready for calculation big multi-digit numbers and floats. Code cleaned.

--- app/js/flex_calc.js
+++ app/js/flex_calc.js
@@ -1,111 +1,158 @@
 /* flex_calc.js –– simple calculator
  *
- *	Adds, substracts, multiplies, divides one-digit integer numbers. Resets prints output.
+ *	Adds, substracts, multiplies, divides integer and float numbers up to 8 digits long. Resets prints output.
  *
- *	TO DO: 	a) process multi-digit numbers (arrays),
- *			b) process float numbers,
- * 			c) output operands and operator / temp sum in "history" output
- *			d) save result to CSV file (for further processing with PHP).
+ *	TO DO: 	d) save result to CSV file (for further processing with PHP).
  *
  */
 
 
-/* Set global vars */
-var result = 0;				// result of the math operation
-var firstOperand = null; 	// first operand
-var operator = null;		// operator +,-,/,%
-var nextOperand = null; 	// second/next operand
-var memory = 0;				// temp for saving result
-var output;					// output result for user
-
-
-// Add listeners to numerals and operators by type   
-document.addEventListener('click', function (event) {
-	
-	// Process numerals
-	if (event.target.matches('.num')) {
-		//console.log(' Number button clicked ');
-		console.log(event.target.innerText);
-
-		// if first operand is null write, to it. if it's defined, write to next/second operand
-		if (firstOperand == null) {
-			firstOperand = Number(event.target.innerText);
-		} else {
-			nextOperand = Number(event.target.innerText);
-		}
-		//console.log('firstOperand: ' + firstOperand + ', nextOperand: ' + nextOperand);
-	}
-
-	// Process operators
-	if (event.target.matches('.opp')) {
-		
-		if (firstOperand == null) {
-			firstOperand = 0;
-		} 
-		//operator = event.target.innerText;
-		operator = event.target.dataset.operator; // get value of 'data-operator' attr of HTML element
-		console.log(operator);
-	}
-}, false);
-
-// Bind actions to other keys when window loads and specify our functions
+// Bind actions to keys when window loads and specify our functions
 window.onload=function(){
-	
-	// Process special keys
-	// Equals – get result of the math operation
-	document.getElementById('btn_equals').addEventListener('click', event => {
-		
-		result = calculate(); // calculate 
-		refreshOutput(); // print output
-		console.log('result: ' + result);
-	});
 
-	// AC - reset
-	document.getElementById('btn_ac').addEventListener('click', event => {
-		
-		result = 0;
-		firstOperand = null;
-		operator = null;
-		nextOperand = null;
-		refreshOutput(); // print output
-		console.log('RESET. Result: ' + result);
-	});
+	/* Set global vars */
+	var result = 0;				// result of the math operation
+	var operand1 = null; 	// first operand
+	var operator = null;		// operator +,-,/,%
+	var operand2 = null; 	// second/next operand
+	var memory = 0;				// temp for saving result
+	var output = document.getElementById('disp_output');					// output result for user
+	var hOutput = document.getElementById('disp_hist');					// output result for user
+	var cFlag = true;			// "clear output" flag
+	var operandReady = 1;		// which operand is ready to assign new value (1 or 2)
 
-	// Save - Save to memory (to be continued – save to CSV file with IP address, browser agent name and datetime)
-	document.getElementById('btn_save').addEventListener('click', event => {
-		// ...
-		memory = result;
-		console.log('SAVED. Result saved: ' + result);
-	});
+	function refreshOutput(){
+
+		// Clear output
+		if (cFlag) {
+			console.log("Output cleared!");
+			output.innerHTML = 0; // clear output
+			hOutput.innerHTML = "&nbsp;";
+		}
+		
+		// Print result on display
+		if (operand1 != null && operand2 != null) {	 
+			if (result.toString().length < 8) { // Handle big numbers
+				output.innerHTML = result;
+				writeHistory();
+			} else {
+				hOutput.innerHTML = "Num too big";
+				output.innerHTML = "ERR"
+			}
+		} 
+	}
+
+	// Write last operation in small display as history
+	function writeHistory(){
+		
+		hOutput.innerHTML = operand1 + " " +  operator + " " + operand2 + " =";
+	}
+
+	// Save value from display to operand 1 or 2
+	function saveVal() {
+		
+		if (operandReady == 1) {
+			operand1 = Number(output.innerHTML);
+			console.log("operand1 saved as " + operand1);
+			operandReady = 2;
+		}  else { //operandReady == 2
+			operand2 = Number(output.innerHTML);
+			console.log("operand2 saved as " + operand2);
+			operandReady = 1;
+		}
+	}
 
 	// Perform math operation on operands
 	function calculate(){
 		
-		console.log('CALCULATE: ' + firstOperand + ' ' + operator + ' ' + nextOperand);
+		console.log('CALCULATE: ' + operand1 + ' ' + operator + ' ' + operand2);
 		switch(operator){
 			case "+": /* plus */ 
-				result = firstOperand + nextOperand;
+				result = operand1 + operand2;
 				break;
 			case "-": /* minus */
-				result = firstOperand - nextOperand;
+				result = operand1 - operand2;
 				break;
 			case "*": /* times / multiply */
-				result = firstOperand * nextOperand;
+				result = operand1 * operand2;
 				break;
 			case "/": /* divide */
-				result = firstOperand / nextOperand;
+				result = operand1 / operand2;
 				break;
 			default: /* unknown operation - print Error */
 				result = "ERR";
 				break;
 		}
 		return result;
-	}
+	}	
 
-	function refreshOutput(){
+	// Add listeners to numerals and operators BY TYPE
+	document.addEventListener('click', function (event) {
+		
+		console.log("operandReady = " + operandReady);
 
-		output = document.getElementById('disp_output');
-		output.innerHTML = result;
+		// Process numbers by type (class) or id
+		if (event.target.matches('.num')) {
+			// Process numerals
+			
+			//console.log(' Number button clicked ');
+			var value = null;
+			if(event.target.innerText){
+				value = event.target.innerText;
+				console.log(value);
+				if (cFlag) {	// Write first number of operand to display
+					output.innerHTML = value;
+					cFlag = false;
+				} else {	// Append next numbers operand to display
+					if(output.innerHTML.length < 8){ // handle big numbers
+						output.innerHTML += value;
+					}
+				} 
+			} else {
+				output.innerHTML = "ERR";
+			}
+		
+		} else if (event.target.matches('.op')) {
+			// Process operators
+			
+			saveVal(); // save value from display
+			operator = event.target.dataset.operator; // get value of 'data-operator' attr of HTML element
+			console.log(operator);
+			cFlag = true; // to clear output first
+		
+		} else if (event.target.matches('#btn_equals')) {	
+			// process Equals – calculate
 
-	}
+			saveVal(); // save value from display
+			if ( operand1 != null && operand1 != null) {
+				result = calculate(); // calculate 
+			} else {
+				result = 0;
+			}
+			refreshOutput(); // print output
+			//output.innerHTML = result;
+			console.log('result: ' + result);
+			cFlag = true;
+		
+		}	else if (event.target.matches('#btn_ac')) { 
+			// process AC – RESET
+
+			result = 0;
+			operand1 = null;
+			operator = null;
+			operand2 = null;
+			operandReady = 1;
+			cFlag = true;
+			refreshOutput(); // print output
+			console.log('RESET. Result: ' + result);
+		
+		} else if (event.target.matches('#btn_save')) {
+			// Save - Save to memory (to be continued – save to CSV file with IP address, browser agent name and datetime)
+	
+			// ...
+			memory = result;
+			console.log('SAVED. Result saved: ' + result);
+		}
+
+	}, false);
 }
